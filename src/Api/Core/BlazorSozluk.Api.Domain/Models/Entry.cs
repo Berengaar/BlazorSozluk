494ce297faddcff2d5a206c40c2662f197113827
Berengaar <emir97han@gmail.com>
2022-10-12 04:03:08 +0300

    Entities created

---
+++ src/Api/Core/BlazorSozluk.Api.Domain/Models/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Api.Domain.Models
+{
+    public class Entry : BaseEntity
+    {
+        public string Subject { get; set; }
+        public virtual User CreatedBy { get; set; }
+        public virtual ICollection<EntryComment> EntryComments { get; set; }
+       
+    }
+}
